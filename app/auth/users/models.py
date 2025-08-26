46e760ad81c480affa9807b11984d31a6dbba2ad
AnupamAryal7 <anmaryal41@gmail.com>
2025-08-26 08:57:46 +0545

    table model for users created

---
+++ app/auth/users/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Text, Integer, Boolean, String
+from sqlalchemy.sql import func
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(40), nullable=False)
+    password = Column(String(40), nullable=False)
+    role = Column(String(20), nullable=False) # "student" "instructor" "admin"
