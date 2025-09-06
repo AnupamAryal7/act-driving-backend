450bac15dcbe070a98dd465fe90a6dfa4796f5cc
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-06 08:52:19 +0545

    models for class session is created

--- app/class_session/models.py
+++ app/class_session/models.py
@@ -6,7 +6,13 @@ from app.database import Base
 
 class ClassSession(Base):
     __tablename__ = "class_session"
-    # id, course id, insturfctor_id , date time, duration, is active, created at, updated at
+    
+    
     id = Column(Integer, primary_key=True, index=True)
     course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
-    instructor_id = Column(Integer, ForeignKey("users.id(roles = instructor)"), nullable=False)
+    instructor_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    date_time = Column(DateTime(timezone=True), nullable=False)
+    duration = Column(Integer, nullable=False)  # Duration in minutes
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
