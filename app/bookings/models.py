5359b730454d292a19373a318b0a20ef3743cfbc
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-07 21:20:01 +0545

    models for booking is created

---
+++ app/bookings/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Boolean
+from sqlalchemy.sql import func
+from app.database import Base
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    class_id = Column(Integer, ForeignKey("class_sessions.id"), nullable=False)
+    status = Column(String(20), nullable=False, default="pending") 
+    remarks = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
