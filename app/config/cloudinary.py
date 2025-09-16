42b899d40d5f2bb9188d0f014ff84b5889bbf1a6
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-16 14:57:30 +0545

    configure_cloudinary function added

---
+++ app/config/cloudinary.py
@@ -0,0 +1,14 @@
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from app.config import settings
+
+def configure_cloudinary():
+    cloudinary.config(
+        cloud_name=settings.CLOUDINARY_CLOUD_NAME,
+        api_key=settings.CLOUDINARY_API_KEY,
+        api_secret=settings.CLOUDINARY_API_SECRET
+    )
+
+# Initialize Cloudinary
+configure_cloudinary()
