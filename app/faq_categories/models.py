355408f65bbd795524bf5f75cf1353c76365c423
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-19 21:13:48 +0545

    model created for faq category

---
+++ app/faq_categories/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String, Integer, Column, Text, DateTime
+from sqlalchemy.sql import func
+from app.database import Base
+
+class Faq_Category(Base):
+    __tablename__ = "faq_categories"
+
+    # id, title, creatd_at, updated_at
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False, unique=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
