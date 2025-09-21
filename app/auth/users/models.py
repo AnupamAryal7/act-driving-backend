b89651244afab2b66158981f680a41d4c8ef77ac
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-21 22:38:44 +0545

    user auth updated

--- app/auth/users/models.py
+++ app/auth/users/models.py
@@ -6,7 +6,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    full_name= Column(String(50), nullable=False)
-    email = Column(String(40), nullable=False)
+    full_name = Column(String(50), nullable=False)
+    email = Column(String(40), nullable=False, unique=True)
+    phone_number = Column(String(15), nullable=True) 
     password = Column(String(100), nullable=False)
-    role = Column(String(20), nullable=False) # "student" "instructor" "admin"
+    role = Column(String(20), nullable=False)
