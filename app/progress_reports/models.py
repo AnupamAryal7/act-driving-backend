f2b0656556a52b4f87ed5693c4dcde0c5da8b2fa
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-30 23:03:02 +0545

    remove unnecessary commits

--- app/progress_reports/models.py
+++ app/progress_reports/models.py
@@ -1,15 +1,12 @@
-#Import 
 from sqlalchemy import Column, Integer, Float, String, Text, DateTime, ForeignKey
 from sqlalchemy.sql import func
 from app.database import Base
 
 
-#Write Logic
 
 class ProgressReport(Base):
     __tablename__ = "progress_reports"
 
-    # id, userr_id, course_id, progress_percentage, status, feedback, remarks, created_at, updated_at
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
